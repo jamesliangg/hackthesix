110a8f0ff8072b04831df34a7d694bbf4078d060
James Liang <86000097+jamesliangg@users.noreply.github.com>
2024-08-03 16:29:39 -0400

    Update video

--- node_test/bokbok/main.js
+++ node_test/bokbok/main.js
@@ -58,7 +58,7 @@ Use the stairs. Your father`
 const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 
 // * Global variables
-let videoURL = "https://www.youtube.com/watch?v=kqFxhfSKZdM";
+let videoURL = "https://www.youtube.com/watch?v=RbVMiu4ubT0";
 
 /**
  * prints confirmation that process has begun
